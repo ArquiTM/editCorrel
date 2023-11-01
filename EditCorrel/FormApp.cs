5e5f421951dc843e6b2c7be111145747e8cfd4a7
Arquimedes Miguel <Arquimedes.Miguel@flex.com>
2023-11-01 12:00:55 -0300

    Fixed created fillDGV method

--- EditCorrel/FormApp.cs
+++ EditCorrel/FormApp.cs
@@ -13,10 +13,13 @@ namespace EditCorrel
         string tempCorrel = string.Empty;
         string newCorrel = string.Empty;
         string line = string.Empty;
+        XmlDocument myDoc = new XmlDocument();
+
         EditXlsx EdX;
         FileImport fI = new FileImport();
-        XmlDocument myDoc = new XmlDocument();
         Utils Uts;
+        FillDGV FDGV;
+
 
         public FormApp()
         {
@@ -30,6 +33,7 @@ namespace EditCorrel
         {
             EdX = new EditXlsx();
             Uts = new Utils();
+            FDGV = new FillDGV();
         }
 
         public static FormApp getInstance()
@@ -159,57 +163,6 @@ namespace EditCorrel
                 textBoxFileVerify.BackColor = Color.Red;
                 textBoxFileVerify.Text = "File Import Error!!!";
                 textBoxFileVerify.ForeColor = Color.White;
-            }
-        }
-
-        private void viewDataGridView()//Fill Data Grid View with values
-        {
-            dataGridViewCorrel.Rows.Clear();
-            if (comboBoxNames.Text == "")
-                MessageBox.Show("Não foi selecionado nenhum arquivo!!!", "ComboBox Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            else
-            {
-                string freqF = string.Empty;
-                string offS = string.Empty;
-                string nodesXml = string.Empty;
-                string[] line;
-                string[] vectLine;
-                int countLine = 0;
-
-                myDoc = new XmlDocument();
-                string file_name = textBoxCorrelDir.Text;
-                myDoc.Load(new StreamReader(file_name));
-
-                foreach (XmlNode node in myDoc.DocumentElement.ChildNodes)
-                {
-                    nodesXml = node.InnerXml;
-
-                    if (nodesXml.Contains(comboBoxNames.Text))
-                    {
-                        line = nodesXml.Split(new[] { "</ValueDescription>", "<ValueDescription />", "</FrequencyOffset>" }, StringSplitOptions.None);
-                        countLine = line.Length;
-
-                        for (int i = 0; i < countLine; i++)
-                        {
-                            if (line[i].Contains("Frequency"))
-                            {
-                                freqF = line[i].Replace("<FrequencyOffset><Frequency>", "");
-                                freqF = freqF.Replace("</Frequency><Offset>", " ");
-                                freqF = freqF.Replace("</Offset>", "");
-                                offS = freqF;
-                                vectLine = freqF.Split(' ');
-
-                                if (vectLine[0].Length == 4)
-                                    vectLine[0] = "0" + vectLine[0];
-
-                                else
-                                    vectLine[0] = "00" + vectLine[0];
-
-                                dataGridViewCorrel.Rows.Add(vectLine[0], vectLine[1]);
-                            }
-                        }
-                    }
-                }
             }
         }
 
@@ -429,7 +382,7 @@ namespace EditCorrel
         {
             if (comboBoxNames.Text != "")
             {
-                viewDataGridView();
+                FDGV.viewDataGridView();
 
                 if (textBoxFreqFileDir.Text != "")
                     EdX.importingExcel(dataGridViewCorrel);
